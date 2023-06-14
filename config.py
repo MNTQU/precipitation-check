578d2a966393d77f3ade138e2d4f1aeb835a788b
MNTQU <myutawinter@yahoo.co.jp>
2023-06-14 14:04:54 +0900

    update config

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ load_dotenv()
 
 #Yahoo API
 yahoo_api_id = os.environ.get("YAHOO_API_ID")
+#slack API
+slack_token = os.environ.get("SLACK_TOKEN")
 #SLACK_CHANNEL
 slack_channel = os.environ.get("SLACK_CHANNEL")
 # 緯度経度
